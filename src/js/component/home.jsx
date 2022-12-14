cb221af3d7a33f0583777bf40139a8ac8c7de401
E-Vivanco <vivanco.elsa@gmail.com>
2022-12-14 19:03:30 +0000

    6° commit act navbar + jumbotron

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,6 @@ import Navbar from "./Navbar";
 //include images into your bundle
 /*import rigoImage from "../../img/rigo-baby.jpg";*/
 
-
 //create your first component
 const Home = () => {
 	return (
